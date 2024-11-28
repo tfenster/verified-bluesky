5525e1c3af37851679853ad87f1a28b96a93df28
Tobias Fenster <tfenster@4psbau.de>
2024-11-28 23:38:14 +0000

    restructure hydration

--- admin/main.go
+++ admin/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
@@ -11,13 +13,18 @@ import (
 	"github.com/shared"
 )
 
+type KVEntry struct {
+	Key   string
+	Value string
+}
+
 func init() {
 
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 
 		case http.MethodPut:
-			// ONLY WORKS FOR MS MVPS AND RDS!
+			// FIXME: THIS ONLY WORKS FOR RDS AND MVPS!
 			adminMode, err := variables.Get("admin_mode")
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusUnauthorized)
@@ -29,8 +36,72 @@ func init() {
 				return
 			}
 
-			fmt.Println("Setting labels")
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer r.Body.Close()
+
+			var kvEntry KVEntry
+			err = json.Unmarshal(body, &kvEntry)
+			if err != nil {
+				http.Error(w, "Error decoding body JSON: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+
 			accessJwt, endpoint, err := shared.LoginToBskyWithReq(r)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			fmt.Println("Setting label for " + kvEntry.Key + " and " + kvEntry.Value)
+
+			store, err := kv.OpenStore("default")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer store.Close()
+
+			valueFromStore, err := store.Get(kvEntry.Key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if string(valueFromStore) != kvEntry.Value {
+				http.Error(w, "Value does not match", http.StatusInternalServerError)
+				return
+			}
+			
+			moduleKey := strings.Split(kvEntry.Key, "-")[0]
+			err = shared.SetLabel("ms-" + moduleKey, string(kvEntry.Value), accessJwt, endpoint)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "text/plain")
+			w.WriteHeader(http.StatusOK)
+
+			fmt.Fprintln(w, "Label ms-" + moduleKey + " set for " + kvEntry.Value)
+
+		case http.MethodGet:
+			adminMode, err := variables.Get("admin_mode")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+				return
+			}
+
+			if adminMode != "true" {
+				http.Error(w, "admin mode not enabled", http.StatusUnauthorized)
+				return
+			}
+
+			fmt.Println("Getting KV entries")
+			_, _, err = shared.LoginToBskyWithReq(r)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusUnauthorized)
 				return
@@ -48,15 +119,27 @@ func init() {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+
+			kvEntries := make([]KVEntry, len(keys))
 			for _, key := range keys {
 				value, err := store.Get(key)
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
 					return
 				}
-				moduleKey := strings.Split(key, "-")[0]
-				shared.SetLabel("ms-" + moduleKey, string(value), accessJwt, endpoint)
+				kvEntries = append(kvEntries, KVEntry{key, string(value)})
 			}
+
+			jsonResult, err := json.Marshal(kvEntries)
+			if err != nil {
+				http.Error(w, "Error encoding result to JSON: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			fmt.Fprintln(w, string(jsonResult))
 
 		case http.MethodDelete:
 			adminMode, err := variables.Get("admin_mode")
