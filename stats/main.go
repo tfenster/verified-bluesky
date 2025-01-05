2ecf853c622c5bd10208159ca9a6170ea0f0b1b0
Tobias Fenster <tfenster@4psbau.de>
2025-01-05 08:45:08 +0100

    chore(stats): more logging for stats (#44)

--- stats/main.go
+++ stats/main.go
@@ -14,21 +14,24 @@ func init() {
 	spinhttp.Handle(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
+			fmt.Println("Getting stats")
 			store, err := kv.OpenStore("default")
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
+				http.Error(w, "Error opening kv store "+err.Error(), http.StatusInternalServerError)
 				return
 			}
 			defer store.Close()
 
+			fmt.Println("Getting keys")
 			keys, err := store.GetKeys()
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
+				http.Error(w, "Error getting keys from kv store "+err.Error(), http.StatusInternalServerError)
 				return
 			}
 
 			typeCounters := make(map[string]int)
 
+			fmt.Println("Counting")
 			for _, key := range keys {
 				keyPrefix := strings.Split(key, "-")[0]
 				if (keyPrefix == "accessJwt") || (keyPrefix == "endpoint") {
@@ -41,6 +44,7 @@ func init() {
 				}
 			}
 
+			fmt.Println("Replying")
 			jsonResult, err := json.Marshal(typeCounters)
 			if err != nil {
 				http.Error(w, "Error encoding result to JSON: "+err.Error(), http.StatusInternalServerError)
